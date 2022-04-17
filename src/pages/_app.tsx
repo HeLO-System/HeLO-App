59e3dbcf656e71da5b70e9a9ae01b6b41da96747
Samuel Höra <samuel@hoera.dev>
2022-04-18 00:49:49 +0200

    ✨ added metatags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,25 @@ const App: FC<AppProps> = ({ Component, pageProps }) => (
   <Layout>
     <Head>
       <title>HeLO-System</title>
+      <meta
+        name="description"
+        content="Hell Let Loose Competitive Clan Ranking"
+      />
+      <meta property="og:url" content="https://helo-system.de/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="HeLO-System" />
+      <meta
+        property="og:description"
+        content="Hell Let Loose Competitive Clan Ranking"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="helo-system.de" />
+      <meta property="twitter:url" content="https://helo-system.de/" />
+      <meta name="twitter:title" content="Helo-System" />
+      <meta
+        name="twitter:description"
+        content="Hell Let Loose Competitive Clan Ranking"
+      />
     </Head>
     <div className="fixed w-screen h-screen -z-10">
       <Image
