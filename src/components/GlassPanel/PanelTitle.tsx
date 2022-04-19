c6c19fb439e418074894c6de9c3da51bedd61eac
Samuel Höra <samuel@hoera.dev>
2022-04-19 23:13:24 +0200

    💄 centered panel title on small viewports

--- src/components/GlassPanel/PanelTitle.tsx
+++ src/components/GlassPanel/PanelTitle.tsx
@@ -8,7 +8,7 @@ interface PanelTitleProps {
 
 export const PanelTitle: FC<PanelTitleProps> = ({ title, className }) => (
   <div className={classNames("text-font flex items-center ", className)}>
-    <hr className="w-10 hidden md:block" />
+    <hr className="w-10 flex-1 md:flex-none" />
     <span className="mx-5 text-xl">{title}</span>
     <hr className="flex-1" />
   </div>
