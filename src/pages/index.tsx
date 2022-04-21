d41c8f8d3796fab7120ce28c21d15923f22bd1b8
Samuel Höra <samuel@hoera.dev>
2022-04-21 21:50:38 +0200

    🐛 fixed topClansByGames displaying topClansByScore instead

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,7 +78,7 @@ const Home: NextPage = () => {
       >
         <div className="grid md:grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-x-10 gap-y-5">
           {topClansByGames
-            ? topClansByScore?.map((clan) => (
+            ? topClansByGames?.map((clan) => (
                 <ClanCard clan={clan} key={clan._id.$oid} />
               ))
             : range(clanRecordLimit).map((index) => <ClanCard key={index} />)}
