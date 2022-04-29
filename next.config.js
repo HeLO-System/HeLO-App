d5d25fa0fe7ca5ac5a4a05f6a8e21d8e44756032
Samuel Höra <samuel@hoera.dev>
2022-04-29 11:07:48 +0200

    🔒️ added security headers

--- next.config.js
+++ next.config.js
@@ -21,6 +21,52 @@ const nextConfig = {
   images: {
     domains: ["cdn.discordapp.com", "media.discordapp.net"],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on"
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload"
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block"
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN"
+          },
+          {
+            key: "Permissions-Policy",
+            value: ""
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff"
+          },
+          {
+            key: "Referrer-Policy",
+            value: "same-origin"
+          },
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'none';
+              script-src 'self' statistics.helo-system.de;
+              style-src 'self';
+              font-src 'self';
+            `
+          }
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
