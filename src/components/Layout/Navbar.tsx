bdb23c0a50c2b21ccda7c12a7fbe6411e6330280
Samuel Höra <samuel@hoera.dev>
2022-06-26 23:08:42 +0200

    made navbar links open in new window (#145)

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -36,12 +36,20 @@ export const NavBar: FC = () => {
           className="bg-transparent shadow-none  hidden md:block"
         ></CustomLink>
         <Link href="https://ko-fi.com/helosystem">
-          <a className="h-10 ml-auto hidden md:flex items-center">
+          <a
+            className="h-10 ml-auto hidden md:flex items-center"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src="/kofi.png" className="h-6" alt="KoFi" />
           </a>
         </Link>
         <Link href="https://github.com/helo-system/">
-          <a className="h-10 w-10 p-2 hidden md:block">
+          <a
+            className="h-10 w-10 p-2 hidden md:block"
+            target="_blank"
+            rel="noreferrer"
+          >
             <GitHub className="fill-white h-full w-auto" />
           </a>
         </Link>
@@ -77,13 +85,17 @@ export const NavBar: FC = () => {
         </li>
         <li className="flex">
           <Link href="https://ko-fi.com/helosystem">
-            <a className="h-10 flex ml-auto items-center">
+            <a
+              className="h-10 flex ml-auto items-center"
+              target="_blank"
+              rel="noreferrer"
+            >
               <span className="my-auto text-white mr-2">Support us</span>
               <img src="/kofi.png" className="h-6" alt="KoFi" />
             </a>
           </Link>
           <Link href="https://github.com/helo-system/">
-            <a className="h-10 w-10 p-2">
+            <a className="h-10 w-10 p-2" target="_blank" rel="noreferrer">
               <GitHub className="fill-white h-full w-auto" />
             </a>
           </Link>
