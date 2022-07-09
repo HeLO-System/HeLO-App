36f91f7448a24a65f3880faf035ad742fcfdb3b1
Samuel Höra <samuel@hoera.dev>
2022-07-09 17:59:15 +0200

    ✨ reimplemented search

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,52 +1,73 @@
 /* eslint-disable @next/next/no-img-element */
 import { CustomLink } from "@components/CustomLink";
+import { Searchbar } from "@components/Search";
+import { SearchPanel } from "@components/Search/SearchPanel";
 import { Navigation24Regular } from "@fluentui/react-icons";
+import { EventListener, useEventListener } from "@hooks";
 import classNames from "classnames";
 import Link from "next/link";
-import { FC, useState } from "react";
+import { FC, useCallback, useState } from "react";
 import Logo from "../../../public/helo.svg";
 import GitHub from "../../../public/mark-github-16.svg";
 
+const navElements: { text: string; href: string }[] = [
+  { text: "Clans", href: "/clans" },
+  { text: "Matches", href: "/matches" },
+  { text: "Statistics", href: "/statistics" },
+  { text: "About", href: "/about" },
+];
+
 export const NavBar: FC = () => {
-  const [open, setOpen] = useState(false);
+  const [navPanelOpen, setNavPanelOpen] = useState(false);
+  const [searchPanelOpen, setSearchPanelOpen] = useState(false);
+
+  const searchPanelKeyListener: EventListener<"keydown"> = useCallback(
+    (e) => {
+      if (e.ctrlKey && e.key === "k") {
+        e.preventDefault();
+        e.stopPropagation();
+
+        setSearchPanelOpen(!searchPanelOpen);
+      }
+    },
+    [searchPanelOpen]
+  );
+
+  useEventListener("keydown", searchPanelKeyListener);
 
   const close = (): void => {
-    setOpen(false);
+    setNavPanelOpen(false);
   };
 
   return (
-    <nav
-      className={classNames("w-full glassmorphism", open ? "h-auto" : "h-10")}
+    <header
+      className={classNames(
+        "w-full glassmorphism",
+        navPanelOpen ? "h-auto" : "h-14"
+      )}
     >
-      <div className="flex w-full">
+      <div className="flex w-full items-center">
         <Link href="/">
           <a>
-            <Logo className="fill-white h-10 w-auto p-2" />
+            <Logo className="fill-white h-14 w-auto p-2" />
           </a>
         </Link>
-        <CustomLink
-          text="Clans"
-          href="/clans"
-          className="bg-transparent shadow-none hidden md:block"
-        ></CustomLink>
-        <CustomLink
-          text="Matches"
-          href="/matches"
-          className="bg-transparent shadow-none  hidden md:block"
-        ></CustomLink>
-        <CustomLink
-          text="Statistics"
-          href="/statistics"
-          className="bg-transparent shadow-none  hidden md:block"
-        ></CustomLink>
-        <CustomLink
-          text="About"
-          href="/about"
-          className="bg-transparent shadow-none  hidden md:block"
-        ></CustomLink>
+        {navElements.map((element, index) => (
+          <CustomLink
+            key={index}
+            className="bg-transparent shadow-none hidden md:block text-xl"
+            {...element}
+          />
+        ))}
+        <Searchbar
+          className="ml-auto"
+          onClick={(): void => {
+            setSearchPanelOpen(!searchPanelOpen);
+          }}
+        />
         <Link href="https://ko-fi.com/helosystem">
           <a
-            className="h-10 ml-auto hidden md:flex items-center"
+            className="h-14 ml-2 hidden md:flex items-center"
             target="_blank"
             rel="noreferrer"
           >
@@ -55,7 +76,7 @@ export const NavBar: FC = () => {
         </Link>
         <Link href="https://github.com/helo-system/">
           <a
-            className="h-10 w-10 p-2 hidden md:block"
+            className="h-14 w-14 p-3 hidden md:block"
             target="_blank"
             rel="noreferrer"
           >
@@ -63,50 +84,29 @@ export const NavBar: FC = () => {
           </a>
         </Link>
         <button
-          onClick={(): void => setOpen(!open)}
-          className="h-10 block md:hidden ml-auto"
+          onClick={(): void => setNavPanelOpen(!navPanelOpen)}
+          className="h-14 block md:hidden"
         >
           <Navigation24Regular className="text-white mr-2" />
         </button>
       </div>
-      <hr className={classNames("h-0 border-e-2", open ? "" : "hidden")} />
-      <ul className={open ? "" : "hidden"}>
-        <li>
-          <CustomLink
-            text="Clans"
-            href="/clans"
-            className="bg-transparent shadow-none !justify-start"
-            onClick={close}
-          ></CustomLink>
-        </li>
-        <li>
-          <CustomLink
-            text="Matches"
-            href="/matches"
-            className="bg-transparent shadow-none !justify-start"
-            onClick={close}
-          ></CustomLink>
-        </li>
-        <li>
-          <CustomLink
-            text="Statistics"
-            href="/statistics"
-            className="bg-transparent shadow-none !justify-start"
-            onClick={close}
-          ></CustomLink>
-        </li>
-        <li>
-          <CustomLink
-            text="About"
-            href="/about"
-            className="bg-transparent shadow-none !justify-start"
-            onClick={close}
-          ></CustomLink>
-        </li>
+      <hr
+        className={classNames("h-0 border-e-2", navPanelOpen ? "" : "hidden")}
+      />
+      <ul className={navPanelOpen ? "" : "hidden"}>
+        {navElements.map((element, index) => (
+          <li key={index}>
+            <CustomLink
+              className="bg-transparent shadow-none !justify-start"
+              onClick={close}
+              {...element}
+            />
+          </li>
+        ))}
         <li className="flex">
           <Link href="https://ko-fi.com/helosystem">
             <a
-              className="h-10 flex ml-auto items-center"
+              className="h-14 flex ml-auto items-center"
               target="_blank"
               rel="noreferrer"
             >
@@ -115,12 +115,19 @@ export const NavBar: FC = () => {
             </a>
           </Link>
           <Link href="https://github.com/helo-system/">
-            <a className="h-10 w-10 p-2" target="_blank" rel="noreferrer">
+            <a className="h-14 w-14 p-2" target="_blank" rel="noreferrer">
               <GitHub className="fill-white h-full w-auto" />
             </a>
           </Link>
         </li>
       </ul>
-    </nav>
+      <SearchPanel
+        isOpen={searchPanelOpen}
+        onRequestClose={(): void => {
+          console.log("test");
+          setSearchPanelOpen(false);
+        }}
+      />
+    </header>
   );
 };
