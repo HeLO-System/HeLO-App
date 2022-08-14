90d5632b35e1dcefc87b8fa85f2587b8c2530dc3
Samuel Höra <samuel@hoera.dev>
2022-08-15 01:21:57 +0200

    updated tacmap form for tacmaps v1.0.0

--- src/components/TacmapForm/TacmapForm.tsx
+++ src/components/TacmapForm/TacmapForm.tsx
@@ -5,23 +5,34 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
+  NumberInput,
+  NumberInputField,
   Select,
   Stack,
 } from "@chakra-ui/react";
 import { MapDirections, Maps, Strongpoints } from "@constants";
 import { Map } from "@types";
+import { numberTransformer } from "@util";
 import { FC, useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
 import { TacmapStrongpointStack } from "./TacMapStrongpointStack";
 
 type TacmapFormProps = {
   setImageUrl: (url: string) => void;
 };
 
+const Colors = z.enum(["red", "blue", ""]);
+type Color = z.infer<typeof Colors>;
+
 export type TacmapUrlForm = {
   map: Map;
   caps: string[];
   hd: boolean;
+  axisColor?: Color;
+  alliesColor?: Color;
+  axisCaps?: number;
+  alliesCaps?: number;
 };
 
 export const TacmapForm: FC<TacmapFormProps> = ({ setImageUrl }) => {
@@ -32,7 +43,15 @@ export const TacmapForm: FC<TacmapFormProps> = ({ setImageUrl }) => {
     watch,
     setValue,
   } = useForm<TacmapUrlForm>({
-    defaultValues: { map: Maps.enum.Carentan, caps: [], hd: false },
+    defaultValues: {
+      map: Maps.enum.Carentan,
+      caps: [],
+      hd: false,
+      axisColor: "red",
+      alliesColor: "blue",
+      axisCaps: 2,
+      alliesCaps: 2,
+    },
   });
   const [capDirections, setCapDirections] = useState<
     [
@@ -46,11 +65,19 @@ export const TacmapForm: FC<TacmapFormProps> = ({ setImageUrl }) => {
   useEffect(() => {
     const strongpoints = fields.caps.filter((cap) => cap).join(",");
     setImageUrl(
-      `https://tacmaps.helo-system.de?map=${fields.map}${
-        strongpoints ? `&strongpoints=${strongpoints}` : ""
-      }${fields.hd ? "" : "&width=960&height=960"}`
+      `https://tacmaps.helo-system.de/tacmap?map=${fields.map}${
+        strongpoints && `&strongpoints=${strongpoints}`
+      }${fields.hd || "&width=960&height=960"}${
+        fields.axisCaps &&
+        fields.axisColor &&
+        `&axisCaps=${fields.axisCaps}&axisColor=${fields.axisColor}`
+      }${
+        fields.alliesCaps &&
+        fields.alliesColor &&
+        `&alliesCaps=${fields.alliesCaps}&axisColor=${fields.alliesColor}`
+      }`
     );
-  }, [fields.caps, fields.hd, fields.map, setImageUrl]);
+  }, [fields, setImageUrl]);
 
   useEffect(() => {
     switch (MapDirections[fields.map]) {
@@ -108,6 +135,104 @@ export const TacmapForm: FC<TacmapFormProps> = ({ setImageUrl }) => {
               key={caps.join()}
             />
           ))}
+        </Stack>
+        <Stack direction="row" w="100%">
+          <Controller
+            name="axisColor"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <FormControl isInvalid={!!errors.axisColor}>
+                <FormLabel htmlFor="axisColor">Axis Color</FormLabel>
+                <Select
+                  id="axisColor"
+                  value={value}
+                  onChange={(event) => {
+                    resetField("caps");
+                    onChange(event);
+                  }}
+                >
+                  {Colors.options.map((color) => (
+                    <option value={color} key={color}>
+                      {color}
+                    </option>
+                  ))}
+                </Select>
+                <FormErrorMessage>
+                  {errors.axisColor && errors.axisColor.message}
+                </FormErrorMessage>
+              </FormControl>
+            )}
+          />
+          <Controller
+            name="axisCaps"
+            control={control}
+            render={({ field: { ref, value, onChange, ...field } }) => (
+              <FormControl isInvalid={!!errors.axisCaps}>
+                <FormLabel htmlFor="axisCaps">Axis Caps</FormLabel>
+                <NumberInput
+                  value={numberTransformer.input(value)}
+                  onChange={(e) => onChange(numberTransformer.output(e))}
+                  {...field}
+                  min={0}
+                  max={5}
+                  id="axisCaps"
+                >
+                  <NumberInputField name="axisCaps" ref={ref} />
+                </NumberInput>
+                <FormErrorMessage>
+                  {errors.axisCaps && errors.axisCaps.message}
+                </FormErrorMessage>
+              </FormControl>
+            )}
+          />
+          <Controller
+            name="alliesCaps"
+            control={control}
+            render={({ field: { ref, value, onChange, ...field } }) => (
+              <FormControl isInvalid={!!errors.alliesCaps}>
+                <FormLabel htmlFor="alliesCaps">Allies Caps</FormLabel>
+                <NumberInput
+                  value={numberTransformer.input(value)}
+                  onChange={(e) => onChange(numberTransformer.output(e))}
+                  {...field}
+                  min={0}
+                  max={5}
+                  id="alliesCaps"
+                >
+                  <NumberInputField name="alliesCaps" ref={ref} />
+                </NumberInput>
+                <FormErrorMessage>
+                  {errors.alliesCaps && errors.alliesCaps.message}
+                </FormErrorMessage>
+              </FormControl>
+            )}
+          />
+          <Controller
+            name="alliesColor"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <FormControl isInvalid={!!errors.alliesColor}>
+                <FormLabel htmlFor="alliesColor">Allies Color</FormLabel>
+                <Select
+                  id="alliesColor"
+                  value={value}
+                  onChange={(event) => {
+                    resetField("caps");
+                    onChange(event);
+                  }}
+                >
+                  {Colors.options.map((color) => (
+                    <option value={color} key={color}>
+                      {color}
+                    </option>
+                  ))}
+                </Select>
+                <FormErrorMessage>
+                  {errors.alliesColor && errors.alliesColor.message}
+                </FormErrorMessage>
+              </FormControl>
+            )}
+          />
         </Stack>
         <ButtonGroup isAttached pr={{ base: 0, md: 8 }}>
           <Button
